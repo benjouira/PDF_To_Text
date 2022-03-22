6fcb4e65911db3e73391c8cdd09f1150f91f2032
Rabii <rabi3jouira@gmail.com>
2022-03-22 15:13:03 +0100

    Create pdfminer.py

---
+++ pdfminer.py
@@ -0,0 +1,5 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.layout import LAParams
+from pdfminer.converter import TextConverter
+from io import StringIO
+from pdfminer.pdfpage import PDFPage
