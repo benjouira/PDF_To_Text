e109fae329e2d3f6cb2e35c4005b027cfacf9345
Rabii <rabi3jouira@gmail.com>
2022-03-22 15:12:26 +0100

    Update PyPDF4.py

--- PyPDF4.py
+++ PyPDF4.py
@@ -2,5 +2,24 @@
 
 from PyPDF4 import PdfFileReader, PdfFileWriter
 
+# **********************
+
 file_path = 'RF210145.pdf'
 pdf = PdfFileReader(file_path)
+
+# *********************
+
+with open ('Lectuer Note.txt','w') as f:
+  for page_num in range(pdf.numPages):
+    print('page: {0}'.format(page_num))
+    pageObj = pdf.getPage(page_num)
+    try:
+      txt = pageObj.extractText()
+      print(''.center(100,'-'))
+    except:
+      pass
+    else:
+      f.write('Page {0}\n'.format(page_num+1))
+      f.write(''.center(100,'-'))
+      f.write(txt)
+  f.close()
