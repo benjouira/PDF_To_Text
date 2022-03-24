736058aff6d0b4bbc005b303f8f13b67ec641ecb
Rabii <rabi3jouira@gmail.com>
2022-03-24 22:47:29 +0100

    Create EasyOCR.py

---
+++ EasyOCR.py
@@ -0,0 +1,14 @@
+!pip install easyocr
+!pip3 install opencv-python
+
+# ************************
+
+import matplotlib.pyplot as plt
+import cv2
+import easyocr
+from pylab import rcParams
+from IPython.display import Image
+rcParams['figure.figsize'] = 8, 16
+
+/**********************
+
