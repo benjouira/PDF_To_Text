faf50f2db6564784e490d4a75ddcc2cffb4ad39f
Rabii <rabi3jouira@gmail.com>
2022-03-31 22:21:16 +0100

    Update slate3k.py

--- slate3k.py
+++ slate3k.py
@@ -10,3 +10,14 @@ print(text)
 # ***************************
 
 # pip install tabula-py
+
+import tabula
+
+
+# Read pdf into a list of DataFrame
+dfs = tabula.read_pdf("MAS_AB02_FÉVRIER_2022.pdf", pages='all')
+dfs
+
+type(dfs[0])
+
+dfs[0].info() 
